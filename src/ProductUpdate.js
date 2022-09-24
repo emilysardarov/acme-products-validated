12cb4096d0aa8c180e8c4990982128574bcef18c
Emily Sardarov <emilysardarov@icloud.com>
2022-09-24 09:28:07 -0500

    can update products

--- src/ProductUpdate.js
+++ src/ProductUpdate.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { updateProduct } from './store';
 
 const ProductUpdate = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { products } = useSelector((state) => state);
   const { id } = useParams();
   const [name, setName] = useState('');
@@ -15,13 +18,27 @@ const ProductUpdate = () => {
     }
   }, [products, id]);
 
+  const update = async (e) => {
+    e.preventDefault();
+    const updated = { id, name };
+    try {
+      dispatch(updateProduct(updated, navigate));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form>
-      <div>
-        <label>Name</label>
-        <input value={name} onChange={(e) => setName(e.target.value)} />
-      </div>
-    </form>
+    <div>
+      <h1>Products</h1>
+      <form onSubmit={update}>
+        <div>
+          <label>Name</label>
+          <input value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <button>Update</button>
+      </form>
+    </div>
   );
 };
 
--- src/Products.js
+++ src/Products.js
@@ -5,19 +5,22 @@ import { Link } from 'react-router-dom';
 const Products = () => {
   const { products } = useSelector((state) => state);
   return (
-    <ul>
-      {products.map((product) => {
-        return (
-          <li key={product.id}>
-            {product.name} (${product.price})
-            <br />
-            In stock: {product.numberInStock}
-            <br />
-            <Link to={`/products/${product.id}`}>Edit</Link>
-          </li>
-        );
-      })}
-    </ul>
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product.id}>
+              {product.name} (${product.price})
+              <br />
+              In stock: {product.numberInStock}
+              <br />
+              <Link to={`/products/${product.id}`}>Edit</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
